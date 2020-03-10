1063fdf5173b39ca98805b28b37db8f167af7d9b
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-03-09 23:24:27 -0600

    edited files

--- app/models/refund.rb
+++ app/models/refund.rb
@@ -1,3 +1,9 @@
 class Refund < ApplicationRecord
   belongs_to :user
+
+  attr_readonly :vessel, :rotNum, :entryNum, :refundof, :enteredAs, :enteredAsQ, :enteredAsV, :enteredAsR, :enteredAsD, :shouldBe, :shouldBeQ, :shouldBeV, :shouldBeR, :shouldBeD, :reasonSupportRefund, :refundClaimed, :date, :signature, :appDate, :compSig, :claimNum, :receivedDate, :claimantSig
+
+
+
+
 end
