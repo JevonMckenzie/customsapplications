9d53527087263895628219028fa31bc289c1de33
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-08-13 20:54:22 -0600

    edited files to allow for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-
+  config.active_job.queue_adapter = :async
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
