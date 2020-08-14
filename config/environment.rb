e9df035c73307a459d039b69f3331035910815f4
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-08-14 16:20:13 -0600

    edited files to fix email issues

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,14 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.smtp_settings = {
+  domain: 'customsapplications.herokuapp.com',
+  address:        "smtp.sendgrid.net",
+  port:            587,
+  authentication: :plain,
+  user_name:      'apikey',
+  password:       ENV['SENDGRID_API_KEY']
+}
+
+
 
