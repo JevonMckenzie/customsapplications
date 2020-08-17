2451b8020e656773bcd7ce2e899ebfbdd1a8b349
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-08-16 21:01:32 -0600

    changed from gmail to outlook

--- app/mailers/postmod_mailer.rb
+++ app/mailers/postmod_mailer.rb
@@ -1,11 +1,11 @@
 class PostmodMailer < ApplicationMailer
 
-  default from: 'jevonmmckenzie@gmail.com'
+  default from: 'systemadministrator1@customs.gov.bz'
 
   def welcome_email
     @postmod = params[:postmod]
     @url  = 'http://customsapplications.herokuapp.com/users/sign_in'
-    mail(to: 'jevonmmckenzie@gmail.com', subject: 'New Request for Post Modification has arrived. Please see summary of details.')
+    mail(to: 'systemadministrator1@customs.gov.bz', subject: 'New Request for Post Modification has arrived. Please see summary of details.')
   end
 end
 	
