5a42fed0ca4d9e90aa79058d4414baf7957f20dc
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-08-19 21:43:50 -0600

    edited files for file upload

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.0'
+gem 'rack-cors'
 gem "paperclip", "~> 5.0.0"
 gem 'sendgrid-ruby'
 gem 'sendgrid-actionmailer'
--- app/mailers/postmod_mailer.rb
+++ app/mailers/postmod_mailer.rb
@@ -1,12 +1,12 @@
 class PostmodMailer < ApplicationMailer
 
-  default from: 'systemadministrator1@customs.gov.bz'
+  default from: 'jevonmmckenzie@gmail.com'
 
   def welcome_email
     @postmod = params[:postmod]
     @url  = 'http://customsapplications.herokuapp.com/users/sign_in'
-    attachments['pdfile_file_name'] = File.read('path/to/file.pdf')
-    mail(to: 'systemadministrator1@customs.gov.bz', subject: 'New Request for Post Modification has arrived. Please see summary of details.')
+    attachments['pdfile_file_name'] = File.read('path/to/pdfile_file_name')
+    mail(to: 'jevonmmckenzie@gmail.com', subject: 'New Request for Post Modification has arrived. Please see summary of details.')
   end
 end
 	
