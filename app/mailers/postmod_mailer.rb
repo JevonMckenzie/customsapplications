6df8fe7d0c1abc3704e585a9b4f6879ce11c77a3
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-08-21 10:09:49 -0600

    edited files to reinclude attachments

--- app/mailers/postmod_mailer.rb
+++ app/mailers/postmod_mailer.rb
@@ -5,7 +5,7 @@ class PostmodMailer < ApplicationMailer
   def welcome_email
     @postmod = params[:postmod]
     @url  = 'http://customsapplications.herokuapp.com/users/sign_in'
-    attachments['pdfile'] = File.read('path/to/pdfile_file')
+    attachments['pdfile'] = File.read('path/to/pdfile')
     mail(to: 'jevonmmckenzie@gmail.com', subject: 'New Request for Post Modification has arrived. Please see summary of details.')
   end
 end
