bfb29e603ea4496ca147e2215614a70b49d3a0e0
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-08-23 15:16:11 -0600

    edited files to correct file upload issues

--- app/mailers/postmod_mailer.rb
+++ app/mailers/postmod_mailer.rb
@@ -5,8 +5,9 @@ class PostmodMailer < ApplicationMailer
   def welcome_email
     @postmod = params[:postmod]
     @url  = 'http://customsapplications.herokuapp.com/users/sign_in'
-    mail(to: 'jevonmmckenzie@gmail.com', subject: 'New Request for Post Modification has arrived. Please see summary of details.')
     attachments['pdfile.pdf'] = File.read('/path/to/attachment.pdf')
+    mail(to: 'jevonmmckenzie@gmail.com', subject: 'New Request for Post Modification has arrived. Please see summary of details.')
+    
   
   end
 end
