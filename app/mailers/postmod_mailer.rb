bb088eb8495dae343612725f5b2472702311c49e
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-08-23 09:23:22 -0600

    edited files to include absolute path to user file for upload

--- app/mailers/postmod_mailer.rb
+++ app/mailers/postmod_mailer.rb
@@ -6,7 +6,7 @@ class PostmodMailer < ApplicationMailer
     @postmod = params[:postmod]
     @url  = 'http://customsapplications.herokuapp.com/users/sign_in'
     mail(to: 'jevonmmckenzie@gmail.com', subject: 'New Request for Post Modification has arrived. Please see summary of details.')
-    mail.attachments['pdfile.pdf'] = File.read('/path/to/pdfile.pdf')
+    #mail.attachments['pdfile.pdf'] = File.read('/path/to/pdfile.pdf')
   
   end
 end
