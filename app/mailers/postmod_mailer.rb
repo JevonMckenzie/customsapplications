397f2924e51566c61d028ef136a4e6982aaffbdb
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-08-28 10:52:48 -0600

    created second method edit_email. not using it as yet

--- app/mailers/postmod_mailer.rb
+++ app/mailers/postmod_mailer.rb
@@ -6,7 +6,9 @@ class PostmodMailer < ApplicationMailer
     #@postmod = params[:postmod]
     @postmod = postmod
     #attachments[@postmod.pdfile_file_name] = "" if @postmod.pdfile_file_name.nil?
-    attachments[@postmod.pdfile_file_name] = File.read(@postmod.pdfile.path ) if @postmod.pdfile_file_name.present?
+
+
+    attachments[@postmod.pdfile_file_name] = File.read(@postmod.pdfile.path ) if @postmod.pdfile.present?
     mail(to: "jevonmmckenzie@gmail.com", subject: "New Request for Post Modification has arrived. Please see summary of details.")
   
     #@url  = 'http://customsapplications.herokuapp.com/users/sign_in'
@@ -15,5 +17,23 @@ class PostmodMailer < ApplicationMailer
     
   
   end
+
+
+
+  def edit_email(postmod)
+    #@postmod = params[:postmod]
+    @postmod = postmod
+    #attachments[@postmod.pdfile_file_name] = "" if @postmod.pdfile_file_name.nil?
+    attachments[@postmod.pdfile_file_name] = File.read(@postmod.pdfile.path ) if @postmod.pdfile.present?
+    mail(to: "jevonmmckenzie@gmail.com", subject: "New Request for Post Modification has arrived. Please see summary of details.")
+  
+    #@url  = 'http://customsapplications.herokuapp.com/users/sign_in'
+    #attachments['pdfile.pdf'] = File.read('/path/to/pdfile.pdf')
+    #mail(to: 'jevonmmckenzie@gmail.com', subject: 'New Request for Post Modification has arrived. Please see summary of details.')
+    
+  
+  end
+
+
 end
 	
