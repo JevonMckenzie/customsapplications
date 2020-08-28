5aef9836da5f1c62e8e6ef04b7424a871f43bb8e
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-08-28 11:00:27 -0600

    created second method edit_email. edited Update method to include it now

--- app/mailers/postmod_mailer.rb
+++ app/mailers/postmod_mailer.rb
@@ -25,7 +25,7 @@ class PostmodMailer < ApplicationMailer
     @postmod = postmod
     #attachments[@postmod.pdfile_file_name] = "" if @postmod.pdfile_file_name.nil?
     attachments[@postmod.pdfile_file_name] = File.read(@postmod.pdfile.path ) if @postmod.pdfile.present?
-    mail(to: "jevonmmckenzie@gmail.com", subject: "New Request for Post Modification has arrived. Please see summary of details.")
+    mail(to: "jevonmmckenzie@gmail.com", subject: "Request for Post Modification has eidited. Please see summary of details.")
   
     #@url  = 'http://customsapplications.herokuapp.com/users/sign_in'
     #attachments['pdfile.pdf'] = File.read('/path/to/pdfile.pdf')
