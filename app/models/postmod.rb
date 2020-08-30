0391068700d39a99ade415853cef7d23cddaec1b
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-08-30 09:20:42 -0600

    edited condition for Receipt Field

--- app/models/postmod.rb
+++ app/models/postmod.rb
@@ -4,39 +4,11 @@ class Postmod < ApplicationRecord
   #attr_accessible :pdfile
   validates_attachment_content_type :pdfile, content_type: ['application/pdf'], :allow_nil => true
   
-  validates :amendfeerecgm, :presence => true, :if => :exporter? 
-  validates :amendfeerecgm, :presence => true, :if => :importer?
-  validates :amendfeerecgm, :presence => true, :if => :noitems?
-  validates :amendfeerecgm, :presence => true, :if => :nopackage?
-  validates :amendfeerecgm, :presence => true, :if => :fccontainernum?
-  validates :amendfeerecgm, :presence => true, :if => :vessel?
-  validates :amendfeerecgm, :presence => true, :if => :location?
-  validates :amendfeerecgm, :presence => true, :if => :nmass?
-  validates :amendfeerecgm, :presence => true, :if => :tainvoice?
-  validates :amendfeerecgm, :presence => true, :if => :description?
-  validates :amendfeerecgm, :presence => true, :if => :commcode?
-  validates :amendfeerecgm, :presence => true, :if => :vinserial?
-  validates :amendfeerecgm, :presence => true, :if => :airbill?
-  validates :amendfeerecgm, :presence => true, :if => :origin?
-  validates :amendfeerecgm, :presence => true, :if => :gmass?
-  validates :amendfeerecgm, :presence => true, :if => :currency?  
-  validates :amendfeerecgm, :presence => true, :if => :iquantity?
-  validates :amendfeerecgm, :presence => true, :if => :procexproccode?
-  validates :amendfeerecgm, :presence => true, :if => :itemvalue?
-  validates :amendfeerecgm, :presence => true, :if => :prevdocrefnum?
-  validates :amendfeerecgm, :presence => true, :if => :bolading?
-  validates :amendfeerecgm, :presence => true, :if => :otherexcert?
 
-  validates :amendfeerec, :presence => true, :if => :transletter?
-  validates :amendfeerec, :presence => true, :if => :invoice?
-  validates :amendfeerec, :presence => true, :if => :vvslip?
-  validates :amendfeerec, :presence => true, :if => :sscertificate?
-  validates :amendfeerec, :presence => true, :if => :otherasdbox?
-  validates :amendfeerec, :presence => true, :if => :popayment?
-  validates :amendfeerec, :presence => true, :if => :certorigin?
-  validates :amendfeerec, :presence => true, :if => :worksheet?
-  validates :amendfeerec, :presence => true, :if => :overlandedcert?
-  validates :amendfeerec, :presence => true, :if => :reason?
+  validates :amendfeerecgm, :presence => true, :if => :recorded?
+
+  validates :amendfeerec, :presence => true, :if => :recorded?
+
 
   validates :reason, :presence => true, :if => :transletter?
   validates :reason, :presence => true, :if => :invoice?
