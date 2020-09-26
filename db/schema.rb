2a9c271776748258500f41780eb96ea9f0aa8458
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-09-26 12:20:48 -0600

    added brokeremail to schema for postmods and edited show to include broker email respective to request

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_140534) do
+ActiveRecord::Schema.define(version: 2020_09_25_162230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2020_08_26_140534) do
     t.boolean "recorded"
     t.string "adminreason"
     t.decimal "ammendcost"
+    t.string "brokeremail"
     t.index ["user_id"], name: "index_postmods_on_user_id"
   end
 
