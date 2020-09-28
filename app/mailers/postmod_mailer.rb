6db397490e9e456f5145917d8be085f2215fd344
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-09-27 19:47:17 -0600

    edited recipients list format

--- app/mailers/postmod_mailer.rb
+++ app/mailers/postmod_mailer.rb
@@ -7,7 +7,7 @@ class PostmodMailer < ApplicationMailer
     @postmod = postmod
     #attachments[@postmod.pdfile_file_name] = "" if @postmod.pdfile_file_name.nil?
     attachments[@postmod.pdfile_file_name] = File.read(@postmod.pdfile.path ) if @postmod.pdfile_file_name.present?
-    mail(to: ['bellcus00@gmail.com, postmod.brokeremail'], subject: "A Post Modification Notification has arrived. Please see summary of details.")
+    mail(to: "bellcus00@gmail.com", "#{@postmod.brokeremail}", subject: "A Post Modification Notification has arrived. Please see summary of details.")
   
     #@url  = 'http://customsapplications.herokuapp.com/users/sign_in'
     #attachments['pdfile.pdf'] = File.read('/path/to/pdfile.pdf')
